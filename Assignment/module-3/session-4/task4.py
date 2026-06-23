962ff995de54f56916718e6b71f55b53cf02d338
Gautamee2006 <gautamikakadiya11@gmail.com>
2026-06-23 09:31:13 +0530

    session-4

--- Assignment/module-3/session-4/task4.py
+++ Assignment/module-3/session-4/task4.py
@@ -4,3 +4,25 @@
  in book_ticket<br> raise InvalidSeatNumberError('Seat number must be positive')
  <br>NameError: name 'InvalidSeatNumberError' is not defined'''
 
+
+
+'''The error occurs because the program tries to raise InvalidSeatNumberError, but this custom 
+exception class has not been defined. Python cannot recognize the name, so it raises a NameError. 
+To fix the error, define the InvalidSeatNumberError class before using it in the raise statement.'''
+
+'''class InvalidSeatNumberError(Exception):
+    pass'''
+
+def book_ticket(movie,seat_no):
+    if seat_no<=0:
+        raise InvalidSeatNumberError("seat number must be positive")
+    else:
+        print("Ticket book sucessfuly for",movie)
+
+movie=input("emter movie name:")
+seat_no=int(input("enter seat number:"))
+
+try:
+    book_ticket(movie,seat_no)
+except InvalidSeatNumberError as e:
+    print(e)
